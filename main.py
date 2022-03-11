054b17473c812c31223c76e23e59392672c2729b
xiewk <1591453921@qq.com>
2022-03-11 20:15:15 +0800

    test

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def main():
+    print('Hello World!')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+    qq_number = '1234567'
+    qq_password = '123'
+
+    print(f"qq_number={qq_number}")
+    print("qq_number={}".format(qq_number))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
