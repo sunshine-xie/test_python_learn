9b11ff915282f8f18c871a369e42a10d8153a5ba
xiewk <1591453921@qq.com>
2022-03-12 14:45:15 +0800

    test8

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
 def print_hi(name):
@@ -19,7 +20,6 @@ if __name__ == '__main__':
 
     price_apple = float(input("苹果的单价 = "))
     weight_apple = float(input("苹果的重量 = "))
-
     money = price_apple * weight_apple
     print(f"money = {money}")
 
@@ -35,5 +35,20 @@ if __name__ == '__main__':
     else:
         print("unable")
 
+    player = int(input("请出拳——石头（1）、剪刀(2)、布（3）= "))
+    computer = random.randint(1, 3)
+    if((player == 1)
+            or (player == 2)
+            or (player == 3)):
+        if ((player == 1 and computer == 2)
+                or (player == 2 and computer == 3)
+                or (player == 3 and computer == 1)):
+            print("玩家获胜")
+        elif (player == 1 and computer == 1) or (player == 2 and computer == 2) or (player == 3 and computer == 3):
+            print("平局")
+        else:
+            print("电脑胜")
+    else:
+        print("输入错误")
 
 
