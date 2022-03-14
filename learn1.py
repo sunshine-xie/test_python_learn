4e14979e8388330b08f68a3a336d02f66bb39517
xiewk <1591453921@qq.com>
2022-03-14 20:25:09 +0800

    test11 函数

--- learn1.py
+++ learn1.py
@@ -1,12 +1,32 @@
-row = 1
+# 循环嵌套
 
-while row <= 9:
-    col = 1
-    while col <= row:
-        print("%d * %d = %d" % (col, row, col * row), end = "   ")
-        col += 1
-    print("")
-    row += 1
+def multiple_table():
+    """
+
+    打印乘法表
+
+    """
+
+    row = 1
+    while row <= 9:
+        col = 1  # 列每次都是从1开始
+        while col <= row:
+            print("%d * %d = %d" % (col, row, col * row), end="\t")
+            col += 1
+        print("")
+        row += 1
+
+
+def sum_2_sum(num1, num2):
+    """
+    两个数加法运算
+    :param num1: 被加数
+    :param num2: 加数
+    :return: 和
+    """
+    result = num1 + num2
+    print("%.3f + %.3f = %.3f" % (num1, num2, result))
+    return result
 
 
 
---
+++ learn2.py
@@ -0,0 +1,17 @@
+# 函数
+
+import learn1
+
+# learn1.multiple_table()
+num1 = float(input("输入 num1 = "))
+num2 = float(input("输入 num2 = "))
+result = learn1.sum_2_sum(num1, num2)
+
+
+
+
+
+
+
+
+
