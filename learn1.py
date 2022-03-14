be649e7f9d488142c9230428cd79514bb65dc100
xiewk <1591453921@qq.com>
2022-03-14 19:31:31 +0800

    test10 乘法表

---
+++ learn1.py
@@ -0,0 +1,12 @@
+row = 1
+
+while row <= 9:
+    col = 1
+    while col <= row:
+        print("%d * %d = %d" % (col, row, col * row), end = "   ")
+        col += 1
+    print("")
+    row += 1
+
+
+
