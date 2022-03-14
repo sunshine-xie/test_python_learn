8ff8d49fc8b8f3a81139a4ce9c1574654fa7fcb0
xiewk <1591453921@qq.com>
2022-03-14 20:41:48 +0800

    test12 函数嵌套调用+注释添加方法+实参、形参

--- learn1.py
+++ learn1.py
@@ -26,7 +26,31 @@ def sum_2_sum(num1, num2):
     """
     result = num1 + num2
     print("%.3f + %.3f = %.3f" % (num1, num2, result))
+
+    multiple_table()
+
     return result
 
+
+def print_line(char, num):
+    """
+    :param char:
+    :param num:
+    """
+    print(char * num)
+
+
+def print_lines(char, num, i):
+    """
+    打印多行
+    :param char: 打印的字符
+    :param num: 打印的个数
+    :param i: 打印的行数
+    :return:
+    """
+    row = 1
+    while row < i:
+        print_line(char, num)
+        row += 1
 
 
--- learn2.py
+++ learn2.py
@@ -3,10 +3,11 @@
 import learn1
 
 # learn1.multiple_table()
-num1 = float(input("输入 num1 = "))
-num2 = float(input("输入 num2 = "))
-result = learn1.sum_2_sum(num1, num2)
+# num1 = float(input("输入 num1 = "))
+# num2 = float(input("输入 num2 = "))
+# result = learn1.sum_2_sum(num1, num2)
 
+learn1.print_lines("~", 50, 10)
 
 
 
